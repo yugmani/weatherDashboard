8a1f29dc8562026fcbc4f9c775d437a93e357965
Himalaya <yugmani@yahoo.com>
2019-11-13 11:53:04 -0800

    refresh button added

--- assets/weather.js
+++ assets/weather.js
@@ -11,80 +11,79 @@ var city = "";
 var today = new Date();
 var date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
 
+//localstorage GetItems
+          var city1 = localStorage.getItem("city1");
+          var city2 = localStorage.getItem("city2");
+          var city3 = localStorage.getItem("city3");
+          var city4 = localStorage.getItem("city4");
+          var city5 = localStorage.getItem("city5");
+          var city6 = localStorage.getItem("city6");
+          $(".city1").html(city1);
+          $(".city2").html(city2);
+          $(".city3").html(city3);
+          $(".city4").html(city4);
+          $(".city5").html(city5);
+          $(".city6").html(city6);
 
-  var city1 = localStorage.getItem("city1");
-  var city2 = localStorage.getItem("city2");
-  var city3 = localStorage.getItem("city3");
-  var city4 = localStorage.getItem("city4");
-  var city5 = localStorage.getItem("city5");
-  var city6 = localStorage.getItem("city6");
-  $(".city1").html(city1);
-  $(".city2").html(city2);
-  $(".city3").html(city3);
-  $(".city4").html(city4);
-  $(".city5").html(city5);
-  $(".city6").html(city6);
+          var currentCity = localStorage.getItem("Current-city");
+          $(".currentCity").html(currentCity);
 
-  var currentCity = localStorage.getItem("Current-city");
-  $(".currentCity").html(currentCity);
+          var todayDate = localStorage.getItem("current-date");  
+          $(".todayDate").html(todayDate);
 
-  var todayDate = localStorage.getItem("current-date");  
-  $(".todayDate").html(todayDate);
+          var description = localStorage.getItem("description");
+          $(".description").html(description);
 
-  var description = localStorage.getItem("description");
-  $(".description").html(description);
+          var weatherIcon = localStorage.getItem("weatherIcon");
+          $(".weatherIcon").attr('src', weatherIcon);
+          
+          var core1 = localStorage.getItem("current-temp");
+          $(".core1").html(core1);
+          
+          var core2 = localStorage.getItem("current-wind");
+          $(".core2").html(core2);
 
-  var weatherIcon = localStorage.getItem("weatherIcon");
-  $(".weatherIcon").attr('src', weatherIcon);
-  // $(".weatherIcon").attr('src') = localStorage.getItem("weatherIcon");
+          var core3 = localStorage.getItem("current-humidity");
+          $(".core3").html(core3);
 
-  var core1 = localStorage.getItem("current-temp");
-  $(".core1").html(core1);
-  
-  var core2 = localStorage.getItem("current-wind");
-  $(".core2").html(core2);
+          var core4 = localStorage.getItem("current-uvIndex");
+          $(".core4").html(core4);
 
-  var core3 = localStorage.getItem("current-humidity");
-  $(".core3").html(core3);
+          for (var i=1; i<6; i++){
+                
+                for (var j=1; j<5; j++) {
+              
+                      if (j===2){
+                        var dayij = localStorage.getItem("day"+i+j);
+                        $(".day"+i+j).attr('src', dayij);
+                      }
+                      else {
+                        var dayij = localStorage.getItem("day"+i+j);
+                        $(".day"+i+j).html(dayij);
+                        
+                      }
+                      var desi = localStorage.getItem("des"+i);
+                      $(".des"+i).html(desi);
+                }//closing 2nd for loop
 
-  var core4 = localStorage.getItem("current-uvIndex");
-  $(".core4").html(core4);
-
-  for (var i=1; i<6; i++){
-    for (var j=1; j<5; j++) {
-  
-          if (j===2){
-            var dayij = localStorage.getItem("day"+i+j);
-            $(".day"+i+j).attr('src', dayij);
-          }
-          else {
-            var dayij = localStorage.getItem("day"+i+j);
-            $(".day"+i+j).html(dayij);
-            
-          }
-          var desi = localStorage.getItem("des"+i);
-          $(".des"+i).html(desi);
-     }//closing 2nd for loop
-} //closing 1st for loop
+          } //closing 1st for loop
 
 
-
-
-//search city 
+//search new city with event listener
 $("#inputImage").click(function (){
+  
+    getStorageItems();
 
-  // removeStorasge();
+    city = $("#searchInput").val();
 
-  city = $("#searchInput").val();
+    // the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=" + APIKey;
 
-// the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=" + APIKey;
-
-// We then created an AJAX call
-      $.ajax({
+    // then created an AJAX call
+    $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
+    }).then(function(response) {
 
         //converting into Fahrenheit measure
         var f = parseInt((response.main.temp - 273.15)*1.80 + 32); 
@@ -94,14 +93,13 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid
         $(".todayDate").html(date);
         localStorage.setItem("current-date", $(".todayDate").text());
         
-      
-
-        $(".city1").html(city);
-        $(".city2").html(city1);
-        $(".city3").html(city2);
-        $(".city4").html(city3);
-        $(".city5").html(city4);
         $(".city6").html(city5);
+        $(".city5").html(city4);
+        $(".city4").html(city3);
+        $(".city3").html(city2);
+        $(".city2").html(city1);
+        $(".city1").html(city);
+        
         localStorage.setItem("city1", $(".city1").text());
         localStorage.setItem("city2", $(".city2").text());
         localStorage.setItem("city3", $(".city3").text());
@@ -109,14 +107,10 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid
         localStorage.setItem("city5", $(".city5").text());
         localStorage.setItem("city6", $(".city6").text());
        
-      var mausam = response.weather[0].main;
-      var lat = response.coord.lat;
-      var lon = response.coord.lon;
+        var mausam = response.weather[0].main;
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
       
-
-      //function call for uv index
-       
-
                 // weather condition checking and matching the weather image;
                 if (mausam === "Clouds") {
                 $(".weatherIcon").attr("src", "images/cloud.png");
@@ -144,201 +138,168 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid
                 }
                 localStorage.setItem("weatherIcon", $(".weatherIcon").prop('src'));
 
-                        $(".description").html(response.weather[0].description);
-                        localStorage.setItem("description", $(".description").text());
-                        $(".core1").html("Temperature: "+f+ "&#176 F");
-                        $(".core2").html("Wind Speed: "+response.wind.speed+"m/s");
-                        $(".core3").html("Humidity: "+response.main.humidity+"%");
-                        localStorage.setItem("current-temp", $(".core1").text());
-                        localStorage.setItem("current-wind", $(".core2").text());
-                        localStorage.setItem("current-humidity", $(".core3").text());
-      //function call for 5 days weather forecast
+        $(".description").html(response.weather[0].description);
+        localStorage.setItem("description", $(".description").text());
+        $(".core1").html("Temperature: "+f+ "&#176 F");
+        $(".core2").html("Wind Speed: "+response.wind.speed+"m/s");
+        $(".core3").html("Humidity: "+response.main.humidity+"%");
+        localStorage.setItem("current-temp", $(".core1").text());
+        localStorage.setItem("current-wind", $(".core2").text());
+        localStorage.setItem("current-humidity", $(".core3").text());
+      
+      //function call for uv index
       uvIndex (lat, lon);
-      foreCast();
-      // storageSetItems();
 
-      }); //ajax close
+      //function call for 5 days weather forecast
+      foreCast();
+      
+
+    }); //ajax close
       
 }); //click function close
 
 
-
-
 //Creating Function for UV Index
 function uvIndex (lat, lon){
-      // var lat= 28.5;
-      // var lon= 27.32;
-
-
+    
       var qURL = "http://api.openweathermap.org/data/2.5/uvi?appid="+APIKey+"&lat="+lat+"&lon="+lon;
 
-
-                $.ajax({
-                  url: qURL,
-                  method: "GET"
-                }).then(function(response) {
-              
+          $.ajax({
+            url: qURL,
+            method: "GET"
+          }).then(function(response) {
+        
                 $(".core4").html("UV Index: "+response.value);
                 localStorage.setItem("current-uvIndex", $(".core4").text());
-                }); //ajax close
+          }); //ajax close
 
 } //uvIndex function close
 
 
 //Creating function for 5 days weather forecast.
 function foreCast(){
-//api.openweathermap.org/data/2.5/forecast?q=Dallas+","+us
-var fURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+APIKey;
 
-      $.ajax({
-        url: fURL,
-        method: "GET"
-      }).then(function(response) {
-        var counter = 1;
+    var fURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+APIKey;
+
+          $.ajax({
+            url: fURL,
+            method: "GET"
+          }).then(function(response) {
+                
+                var counter = 1;
           
-        for (var i=3; i<=40; i=i+8){
-              var ft = parseInt((response.list[i].main.temp - 273.15)*1.80 + 32);
+                for (var i=3; i<=40; i=i+8){
+
+                      //conversion to fahrenheit
+                      var ft = parseInt((response.list[i].main.temp - 273.15)*1.80 + 32);
+              
+                      $(".day"+counter+"1").html((response.list[i].dt_txt).substring(0, 11));
+                      localStorage.setItem("day"+counter+"1", $(".day"+counter+"1").text());
+                      
+                      var mausami = response.list[i].weather[0].main;
+                                  
+                            if (mausami === "Clouds") {
+                            $(".day"+counter+"2").attr("src", "images/cloud.png");
+                            }
+                            else if (mausami === "Rain") {
+                              $(".day"+counter+"2").attr("src", "images/rain.png");
+                            }
+                            else if (mausami === "Drizzle") {
+                              $(".day"+counter+"2").attr("src", "images/drizzle.png");
+                            }
+
+                            else if (mausami === "Clear") {
+                              $(".day"+counter+"2").attr("src", "images/clear.png");
+                            }
+                            else if (mausami === "Snow") {
+                              $(".day"+counter+"2").attr("src", "images/snow.png");
+                            }
+
+                            else if (mausami === "Thunderstorm") {
+                              $(".day"+counter+"2").attr("src", "images/thunder.png");
+                            }
+
+                            else   {
+                              $(".day"+counter+"2").attr("src", "images/Cloudy-128.png");
+                            }
+
+                      localStorage.setItem("day"+counter+"2", $(".day"+counter+"2").prop('src'));
+                      
+                      $(".des"+counter).html(response.list[i].weather[0].description)
+                      localStorage.setItem("des"+counter, $(".des"+counter).text());
+                      $(".day"+counter+"3").html("Temperature: "+ft+"&#176 F");
+                      localStorage.setItem("day"+counter+"3", $(".day"+counter+"3").text());
+                      $(".day"+counter+"4").html("Humidity: "+response.list[i].main.humidity+"%");
+                      localStorage.setItem("day"+counter+"4", $(".day"+counter+"4").text());
+
+                      counter = counter + 1;
+
+                } //for loop closing
+
+          }); //ajax closing
+     
+  } //5days weather forecast function close
+
+// creating function for localStorage GetItems
+function getStorageItems(){
+
+      var city1 = localStorage.getItem("city1");
+      var city2 = localStorage.getItem("city2");
+      var city3 = localStorage.getItem("city3");
+      var city4 = localStorage.getItem("city4");
+      var city5 = localStorage.getItem("city5");
+      var city6 = localStorage.getItem("city6");
+            $(".city1").html(city1);
+            $(".city2").html(city2);
+            $(".city3").html(city3);
+            $(".city4").html(city4);
+            $(".city5").html(city5);
+            $(".city6").html(city6);
+
+      var currentCity = localStorage.getItem("Current-city");
+      $(".currentCity").html(currentCity);
+
+      var todayDate = localStorage.getItem("current-date");  
+      $(".todayDate").html(todayDate);
+
+      var description = localStorage.getItem("description");
+      $(".description").html(description);
+
+      var weatherIcon = localStorage.getItem("weatherIcon");
+      $(".weatherIcon").attr('src', weatherIcon);
       
-    
-              $(".day"+counter+"1").html((response.list[i].dt_txt).substring(0, 11));
-              localStorage.setItem("day"+counter+"1", $(".day"+counter+"1").text());
+      var core1 = localStorage.getItem("current-temp");
+      $(".core1").html(core1);
+      
+      var core2 = localStorage.getItem("current-wind");
+      $(".core2").html(core2);
+
+      var core3 = localStorage.getItem("current-humidity");
+      $(".core3").html(core3);
+
+      var core4 = localStorage.getItem("current-uvIndex");
+      $(".core4").html(core4);
+
+            for (var i=1; i<6; i++){
+                    for (var j=1; j<5; j++) {
+                  
+                          if (j===2){
+                            var dayij = localStorage.getItem("day"+i+j);
+                            $(".day"+i+j).attr('src', dayij);
+                          }
+                          else {
+                            var dayij = localStorage.getItem("day"+i+j);
+                            $(".day"+i+j).html(dayij);
+                            
+                          }
+                          var desi = localStorage.getItem("des"+i);
+                          $(".des"+i).html(desi);
+                    
+                    }//closing 2nd for loop
               
-              var mausami = response.list[i].weather[0].main;
-              // console.log(mausami);
-     
-                    if (mausami === "Clouds") {
-                    $(".day"+counter+"2").attr("src", "images/cloud.png");
-                    }
-                    else if (mausami === "Rain") {
-                      $(".day"+counter+"2").attr("src", "images/rain.png");
-                    }
-                    else if (mausami === "Drizzle") {
-                      $(".day"+counter+"2").attr("src", "images/drizzle.png");
-                    }
+            } //closing 1st for loop
 
-                    else if (mausami === "Clear") {
-                      $(".day"+counter+"2").attr("src", "images/clear.png");
-                    }
-                    else if (mausami === "Snow") {
-                      $(".day"+counter+"2").attr("src", "images/snow.png");
-                    }
-
-                    else if (mausami === "Thunderstorm") {
-                      $(".day"+counter+"2").attr("src", "images/thunder.png");
-                    }
-
-                    else   {
-                      $(".day"+counter+"2").attr("src", "images/Cloudy-128.png");
-                    }
-                    localStorage.setItem("day"+counter+"2", $(".day"+counter+"2").prop('src'));
-              
-              $(".des"+counter).html(response.list[i].weather[0].description)
-              localStorage.setItem("des"+counter, $(".des"+counter).text());
-              $(".day"+counter+"3").html("Temperature: "+ft+"&#176 F");
-              localStorage.setItem("day"+counter+"3", $(".day"+counter+"3").text());
-              $(".day"+counter+"4").html("Humidity: "+response.list[i].main.humidity+"%");
-              localStorage.setItem("day"+counter+"4", $(".day"+counter+"4").text());
-
-              counter = counter + 1;
-
-        } //for loop closing
-
-      }); //ajax closing
-     
-    } //5days weather forecast function close
-
-
-//function to set items in localstorage 
-// function storageSetItems(){
-
-//         localStorage.setItem("Current-city", $(".currentCity").text());
-//         localStorage.setItem("current-date", $(".todayDate").text());
-//         localStorage.setItem("description", $(".description").text());
-//         localStorage.setItem("weatherIcon", $(".weatherIcon").prop('src'));
-//         localStorage.setItem("current-temp", $(".core1").text());
-//         localStorage.setItem("current-wind", $(".core2").text());
-//         localStorage.setItem("current-humidity", $(".core3").text());
-//         // localStorage.setItem("current-uvIndex", $(".core4").text());
-//        localStorage.setItem("city1", $(".city1").text());
-//        localStorage.setItem("city2", $(".city2").text());
-//        localStorage.setItem("city3", $(".city3").text());
-//        localStorage.setItem("city4", $(".city4").text());
-//        localStorage.setItem("city5", $(".city5").text());
-//        localStorage.setItem("city6", $(".city6").text());
-       
-        
-
-//       } //closing storageSetItems function
-
-
-    
-// function getStorageItems(){
-
-//   var city1 = localStorage.getItem("city1");
-//   var city2 = localStorage.getItem("city2");
-//   var city3 = localStorage.getItem("city3");
-//   var city4 = localStorage.getItem("city4");
-//   var city5 = localStorage.getItem("city5");
-//   var city6 = localStorage.getItem("city6");
-//   $(".city1").html(city1);
-//   $(".city2").html(city2);
-//   $(".city3").html(city3);
-//   $(".city4").html(city4);
-//   $(".city5").html(city5);
-//   $(".city6").html(city6);
-
-//   var currentCity = localStorage.getItem("Current-city");
-//   $(".currentCity").html(currentCity);
-
-//   var todayDate = localStorage.getItem("current-date");  
-//   $(".todayDate").html(todayDate);
-
-//   var description = localStorage.getItem("description");
-//   $(".description").html(description);
-
-//   var weatherIcon = localStorage.getItem("weatherIcon");
-//   $(".weatherIcon").attr('src', weatherIcon);
-//   // $(".weatherIcon").attr('src') = localStorage.getItem("weatherIcon");
-
-//   var core1 = localStorage.getItem("current-temp");
-//   $(".core1").html(core1);
-  
-//   var core2 = localStorage.getItem("current-wind");
-//   $(".core2").html(core2);
-
-//   var core3 = localStorage.getItem("current-humidity");
-//   $(".core3").html(core3);
-
-//   var core4 = localStorage.getItem("current-uvIndex");
-//   $(".core4").html(core4);
-
-//   for (var i=1; i<6; i++){
-//     for (var j=1; j<5; j++) {
-  
-//           if (j===2){
-//             var dayij = localStorage.getItem("day"+i+j);
-//             $(".day"+i+j).attr('src', dayij);
-//           }
-//           else {
-//             var dayij = localStorage.getItem("day"+i+j);
-//             $(".day"+i+j).html(dayij);
-//           }
-
-//      }//closing 2nd for loop
-//         } //closing 1st for loop
-
-
-
-//       } //closing getStorageItems function
-
-
-
-
-
-
-
-
-
+} //closing getStorageItems function
 
 
 
